dcfeb4ea1ec0c472c60b5417112978f8e03240f5
Oleg Korolenko <olegkorolenko@MacBook-Air-Oleg.local>
2019-10-18 21:32:13 +0300

    Make function

--- run.php
+++ run.php
@@ -6,39 +6,50 @@
  * Time: 11:02
  */
 
-$db = new mysqli(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE, 3306);
-$db->set_charset("utf8");
-if ($db->connect_errno) {
-    die("No connect to db");
-}
-echo '<pre>';
-$sqlTables = "SHOW TABLES";
-$sqlManufacturers = "select keyword from ukr_url_alias where query like 'manufacturer_id%'";
-
-
-$tables = $db->query($sqlTables);
-while ($table = $tables->fetch_row()) {
-    $tablesArray[] = $table;
-}
-
-foreach ($tablesArray as $table) {
-    echo '<h1>' . 'Table ' . $table[0] . ' start!' . '</h1>' . PHP_EOL;
-    $fieldsSql = "SHOW COLUMNS FROM " . $table[0];
-
-    $field = $db->query($fieldsSql);
-    while ($fieldData = $field->fetch_assoc()) {
-        $fields[] = $fieldData;
+/*
+ * @param string $host Db host
+ * @param string $user Db user
+ * @param string $password Db password
+ * @param string $database Db name
+ * @param int $port
+ * @param string $from Search String
+ * @param string $to Replace String
+ * @return null
+ */
+function SearchAndReplace($host, $user, $password, $database, $port = 3306, $from, $to)
+{
+    $db = new mysqli($host, $user, $password, $database, $port);
+    $db->set_charset("    utf8");
+    if ($db->connect_errno) {
+        die("No connect to db");
     }
-    foreach ($fields as $field) {
-        if (substr($field['Type'], 0, 7) == 'varchar' || substr($field['Type'], 0, 4) == 'text') {
-            $query = /** @lang SQL */
-                "UPDATE " . $table[0] . " SET " . $field['Field'] . " = REPLACE(" . $field['Field'] . ", 'http://e-ukrservice.com', 'https://e-ukrservice.com')";
-            echo $query;
-            $db->query($query);
-            echo 'Field ' . $field['Field'] . '- Done!!!' . PHP_EOL;
+    echo '<pre>';
+    $sqlTables = "SHOW TABLES";
+
+    $tables = $db->query($sqlTables);
+    while ($table = $tables->fetch_row()) {
+        $tablesArray[] = $table;
+    }
+
+    foreach ($tablesArray as $table) {
+        echo '<h1>' . 'Table ' . $table[0] . ' start!' . '</h1>' . PHP_EOL;
+        $fieldsSql = "SHOW COLUMNS FROM " . $table[0];
+
+        $field = $db->query($fieldsSql);
+        while ($fieldData = $field->fetch_assoc()) {
+            $fields[] = $fieldData;
         }
+        foreach ($fields as $field) {
+            if (substr($field['Type'], 0, 7) == 'varchar' || substr($field['Type'], 0, 4) == 'text') {
+                $query = /** @lang SQL */
+                    "UPDATE " . $table[0] . " SET " . $field['Field'] . " = REPLACE(" . $field['Field'] . ", $from, $to)";
+                echo $query;
+                $db->query($query);
+                echo 'Field ' . $field['Field'] . '- Done!!!' . PHP_EOL;
+            }
+        }
+
     }
-
+    $db->close();
+    echo 'Executed';
 }
-
-$db->close();
